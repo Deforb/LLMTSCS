1584a071a907137c8b05c742400d8e7868ff50a5
Deforb <2811694664@qq.com>
2025-03-18 18:59:18 +0800

    merge vllm to run_open_llm

--- run_open_LLM.py
+++ run_open_LLM.py
@@ -2,15 +2,17 @@ import os
 import time
 import argparse
 from utils import error
-from utils.llm_aft_trainer import LLM_Inference
 from utils.config import *
 from utils.utils import merge
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--memo", type=str, default='LLMTLCSRun')
     parser.add_argument("--llm_model", type=str, default="llama_cgpr_13b_jinan_1")
-    parser.add_argument("--llm_path", type=str, default="./ft_models/merged/llama_cgpr_13b_jinan_1")
+    parser.add_argument(
+        "--llm_path", type=str, default="./ft_models/merged/llama_cgpr_13b_jinan_1"
+    )
     parser.add_argument("--num_rounds", type=int, default=1)
     parser.add_argument("--new_max_tokens", type=int, default=1024)
     parser.add_argument("--proj_name", type=str, default="LLM-TSCS-extreme")
@@ -19,6 +21,7 @@ def parse_args():
     parser.add_argument("--workers", type=int, default=1)
     parser.add_argument("--dataset", type=str, default="template")
     parser.add_argument("--traffic_file", type=str, default="flow_main_stream.json")
+    parser.add_argument("--with_vllm", type=bool, default=False)
 
     return parser.parse_args()
 
@@ -29,14 +32,22 @@ def main(in_args):
     if in_args.dataset == 'jinan':
         count = 3600
         road_net = "3_4"
-        traffic_file_list = ["anon_3_4_jinan_real.json", "anon_3_4_jinan_real_2000.json",
-                             "anon_3_4_jinan_real_2500.json", "anon_3_4_jinan_synthetic_24000_60min.json",
-                             "anon_3_4_jinan_synthetic_24h_6000.json"]
+        traffic_file_list = [
+            "anon_3_4_jinan_real.json",
+            "anon_3_4_jinan_real_2000.json",
+            "anon_3_4_jinan_real_2500.json",
+            "anon_3_4_jinan_synthetic_24000_60min.json",
+            "anon_3_4_jinan_synthetic_24h_6000.json",
+        ]
         template = "Jinan"
     elif in_args.dataset == 'hangzhou':
         count = 3600
         road_net = "4_4"
-        traffic_file_list = ["anon_4_4_hangzhou_real.json", "anon_4_4_hangzhou_real_5816.json", "anon_4_4_hangzhou_synthetic_24000_60min.json"]
+        traffic_file_list = [
+            "anon_4_4_hangzhou_real.json",
+            "anon_4_4_hangzhou_real_5816.json",
+            "anon_4_4_hangzhou_synthetic_24000_60min.json",
+        ]
         template = "Hangzhou"
     elif in_args.dataset == 'newyork_16x3':
         count = 3600
@@ -46,7 +57,10 @@ def main(in_args):
     elif in_args.dataset == 'newyork_28x7':
         count = 3600
         road_net = "28_7"
-        traffic_file_list = ["anon_28_7_newyork_real_double.json", "anon_28_7_newyork_real_triple.json"]
+        traffic_file_list = [
+            "anon_28_7_newyork_real_double.json",
+            "anon_28_7_newyork_real_triple.json",
+        ]
         template = "NewYork"
     in_args.model = in_args.memo
 
@@ -70,31 +84,25 @@ def main(in_args):
         "LLM_PATH": in_args.llm_path,
         "LLM_MODEL": in_args.llm_model,
         "LOG_DIR": f"./{in_args.llm_model}_logs",
-        "NEW_MAX_TOKENS": in_args.new_max_tokens
+        "NEW_MAX_TOKENS": in_args.new_max_tokens,
     }
 
     dic_traffic_env_conf_extra = {
         "NUM_AGENTS": num_intersections,
         "NUM_INTERSECTIONS": num_intersections,
-
         "MODEL_NAME": f"{in_args.model}-{dic_agent_conf_extra['LLM_MODEL']}",
         "PROJECT_NAME": in_args.proj_name,
         "RUN_COUNTS": count,
         "NUM_ROUNDS": in_args.num_rounds,
         "NUM_ROW": NUM_ROW,
         "NUM_COL": NUM_COL,
-
         "TRAFFIC_FILE": in_args.traffic_file,
         "ROADNET_FILE": "roadnet_{0}.json".format(road_net),
-
         "LIST_STATE_FEATURE": [
             "cur_phase",
             "traffic_movement_pressure_queue",
         ],
-
-        "DIC_REWARD_INFO": {
-            'queue_length': -0.25
-        }
+        "DIC_REWARD_INFO": {'queue_length': -0.25},
     }
 
     if in_args.eightphase:
@@ -106,30 +114,67 @@ def main(in_args):
             5: [1, 1, 0, 0, 0, 0, 0, 0],
             6: [0, 0, 1, 1, 0, 0, 0, 0],
             7: [0, 0, 0, 0, 0, 0, 1, 1],
-            8: [0, 0, 0, 0, 1, 1, 0, 0]
+            8: [0, 0, 0, 0, 1, 1, 0, 0],
         }
-        dic_traffic_env_conf_extra["PHASE_LIST"] = ['WT_ET', 'NT_ST', 'WL_EL', 'NL_SL',
-                                                    'WL_WT', 'EL_ET', 'SL_ST', 'NL_NT']
+        dic_traffic_env_conf_extra["PHASE_LIST"] = [
+            'WT_ET',
+            'NT_ST',
+            'WL_EL',
+            'NL_SL',
+            'WL_WT',
+            'EL_ET',
+            'SL_ST',
+            'NL_NT',
+        ]
         dic_agent_conf_extra["FIXED_TIME"] = [30, 30, 30, 30, 30, 30, 30, 30]
 
     else:
         dic_agent_conf_extra["FIXED_TIME"] = [30, 30, 30, 30]
 
-    dic_traffic_env_conf_extra["NUM_AGENTS"] = dic_traffic_env_conf_extra["NUM_INTERSECTIONS"]
+    dic_traffic_env_conf_extra["NUM_AGENTS"] = dic_traffic_env_conf_extra[
+        "NUM_INTERSECTIONS"
+    ]
     dic_path_extra = {
-        "PATH_TO_MODEL": os.path.join("model", in_args.memo, in_args.traffic_file + "_" +
-                                      time.strftime('%m_%d_%H_%M_%S', time.localtime(time.time()))),
-        "PATH_TO_WORK_DIRECTORY": os.path.join("records", in_args.memo, in_args.traffic_file + "_" +
-                                               time.strftime('%m_%d_%H_%M_%S', time.localtime(time.time()))),
-        "PATH_TO_DATA": os.path.join("data", template, str(road_net))
+        "PATH_TO_MODEL": os.path.join(
+            "model",
+            in_args.memo,
+            in_args.traffic_file
+            + "_"
+            + time.strftime('%m_%d_%H_%M_%S', time.localtime(time.time())),
+        ),
+        "PATH_TO_WORK_DIRECTORY": os.path.join(
+            "records",
+            in_args.memo,
+            in_args.traffic_file
+            + "_"
+            + time.strftime('%m_%d_%H_%M_%S', time.localtime(time.time())),
+        ),
+        "PATH_TO_DATA": os.path.join("data", template, str(road_net)),
     }
 
-    trainer = LLM_Inference(dic_agent_conf_extra,
-                            merge(dic_traffic_env_conf, dic_traffic_env_conf_extra),
-                            dic_path_extra,
-                            f'{template}-{road_net}', in_args.traffic_file.split(".")[0])
+    if in_args.with_vllm:
+        from utils.llm_aft_trainer import LLM_Inference_VLLM
+
+        trainer = LLM_Inference_VLLM(
+            dic_agent_conf_extra,
+            merge(dic_traffic_env_conf, dic_traffic_env_conf_extra),
+            dic_path_extra,
+            f'{template}-{road_net}',
+            in_args.traffic_file.split(".")[0],
+        )
+    else:
+        from utils.llm_aft_trainer import LLM_Inference
+
+        trainer = LLM_Inference(
+            dic_agent_conf_extra,
+            merge(dic_traffic_env_conf, dic_traffic_env_conf_extra),
+            dic_path_extra,
+            f'{template}-{road_net}',
+            in_args.traffic_file.split(".")[0],
+        )
 
     trainer.train_test()
+
 
 if __name__ == "__main__":
     args = parse_args()
